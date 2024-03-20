898f85180df86c3fcecbe5bc20034cf4dc753019
ryann0097 <ryandavidrsw@gmail.com>
2024-03-19 21:31:58 -0300

    lp1graal

--- author.md
+++ author.md
@@ -2,22 +2,94 @@
 
 Preencha os dados abaixo para identificar a autoria do trabalho.
 
-- Nome: *<insira seu nome aqui>*
-- Email: *<insira seu email aqui>*
-- Turma: *<insira sua turma aqui>*
+- Nome: *<Ryan David dos Santos Silvestre>*
+- Email: *<ryan.silvestre.718@ufrn.edu.br>*
+- Turma: *<DIM0176 - T01>*
 
 # Questões Finalizadas
 
-- [ ] minmax
-- [ ] reverse
-- [ ] copy
-- [ ] find_if
-- [ ] all_of
-- [ ] any_of
-- [ ] none_of
-- [ ] equal
-- [ ] unique
-- [ ] partition
+- [X] minmax
+- [X] reverse
+- [X] copy
+- [X] find_if
+- [X] all_of
+- [X] any_of
+- [X] none_of
+- [X] equal
+- [X] unique
+- [X] partition
 
---------
+--------------------------------------------
+
+Comentários do autor:
+    
+    Professor, tive algumas dúvidas sobre as funções UNIQUE e a primeira sobrecarga da EQUAL.
+    Antecipadamente, peço desculpas por qualquer incômodo que minhas muitas mensagens possam ter causado. Como falei 
+    no Discord outro dia, tive uma semana muito corrida, apenas me disponibilizando algum tempo durante o final de semana
+    até o dia de entrega. Como entraves nesse primeiro projeto, tive a falta de tempo hábil devido a outras atividades extracurriculares,
+    outras matérias e trabalho, falta de conhecimento prévio da ferramenta "CMake", falta de conhecimento prévio da ferramenta "Git".
+    Porém, fico feliz em informar que consegui contornar esses problemas e estar apresentando o presente trabalho ao senhor.
+    Creio que nos compromissamos a pagar a matéria com o senhor por livre e espontânea vontade, não devendo, depois, reclamar
+    do que nós mesmos escolhemos. Nossa melhor opção é dar nosso melhor e evitar ao máximo qualquer tipo de desculpas.
+    
+     - Sobre a EQUAL, falei com o senhor na aula de 19/03 sobre o problema, mas não consegui resolver. 
+    Procurei alguns tópicos sobre aritmética de ponteiros, tentei implementações com somas e incrementos, tudo sem sucesso.
+    Daí, falei com a colega de sala "Beatriz Camilo" e comparamos nossos códigos. Vi que apenas estava usando uma abordagem
+    errada, mas estava no caminho certo. Aqui está minha abordagem inicial após falar com o senhor:
+
+    template <typename Itr, typename Equal>
+    bool equal (Itr first1, Itr last1, Itr first2, Equal eq){
+        int Iterador = 0;
+        while(first1 != last1){
+            Iterador++;
+        }
+        Itr FullRange = (last1 - first1);
+        Itr HalfRange = first2;
+        for(int i = 0; i < Iterador; i++){
+            HalfRange++;
+        }
+    return eq(FullRange, HalfRange);
+    }
+
+    Após ver como estava a função dela, percebi que eu só teria de usar uma abordagem semelhante à das outras questões, não inventar
+    coisas do nada.
+
+-----------------------------------------------
+
+    - Sobre a UNIQUE, inicialmente utilizei a abordagem vista em sala da FILTEr, em que abordávamos a questão dos ponteiros lentos
+    e rápidos. Porém, ao tentar compilar, vi que tinha alguns erros e dei uma olhada do Copilot sobre como resolver.
+    template <typename Itr, typename Equal> 
+    
+    <!--Código Inicial-->
+    Itr unique(Itr first, Itr last, Equal eq) {
+    Itr pointerOne = first;
+    Itr pointerTwo = first;
+
+    while(pointerOne != last){
+        for(auto it = first; it != pointerOne; it++){
+        if(!(eq(*pointerOne, *it))){  
+                *pointerTwo = *pointerOne;
+                pointerTwo++;
+                }
+            }
+        pointerOne++;
+    }
+        return pointerTwo;
+    } 
+
+    <!--Código Final-->
+    template <typename Itr, typename Equal> 
+    Itr unique(Itr first, Itr last, Equal eq) {
+    if (first == last) return last;
+
+    Itr pointerOne = first;
+    while (++first != last) {
+        if (!eq(*pointerOne, *first)) {
+            *(++pointerOne) = *first;
+        }
+    }
+    return ++pointerOne;
+    }
+
+
 &copy; DIMAp/UFRN 2022.
--- include/graal.h
+++ include/graal.h
@@ -10,10 +10,10 @@ namespace graal {
  * Finds and returns a pair with the smallest and greatest elements in a range.
  *
  * @tparam Itr Iterator to the range.
- * @tparam Compare A regular comparison function.Comparison functor.
+ * @tparam Compare A regular comparison function. Comparison functor.
  *
- * @param first_ Pointer to the first element of the range we want to copy (inclusive).
- * @param last_ Pointer to the last element of the range we want to copy (exclusive).
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
  * @param cmp The comparison function that return true if the first element is *less* than the
  * second.
  *
@@ -33,71 +33,74 @@ template< typename Itr, typename Compare >
    else{
     for(auto it = first; it != last; it++){
      if(cmp(*it, *pointerOne)){pointerOne = it;}
-     if(cmp(*pointerTwo, *it) || it >= pointerTwo){pointerTwo = it;}
+     if(cmp(*pointerTwo, *it) || *it >= *pointerTwo){pointerTwo = it;}
    }
  }
  return std::make_pair(pointerOne, pointerTwo);
 }
 
-/**
- * 
- * Doxygen reverse
- * 
- * 
- * 
- * 
- * 
-*/
-
+/*!
+ * Reverses the order of the elements in a range.
+ *
+ * @tparam Itr Iterator to the range.
+ *
+ * @param first_ Pointer to the first element of the range we want to reverse (inclusive).
+ * @param last_ Pointer to the last element of the range we want to reverse (exclusive).
+ *
+ */
 
 template <typename Itr> 
 void reverse(Itr first, Itr last) {
-    auto Alcance = last - first;
-    for (int i = 0; i < Alcance / 2; i++) {
-        auto Troca = Alcance - i - 1;
-        auto Temp = *(first + i);
-        *(first + i) = *(first + Troca);
-        *(first + Troca) = Temp;
-    }
+  auto Alcance = last - first;
+  for (int i = 0; i < Alcance / 2; i++) {
+    auto Troca = Alcance - i - 1;
+    auto Temp = *(first + i);
+    *(first + i) = *(first + Troca);
+    *(first + Troca) = Temp;
+  }
 }
 
-/**
- * 
- * 
- * 
- * doxygen copy
- * 
- * 
- * 
- * 
-*/
+/*!
+ * Copies the range values into a new range beginning.
+ *
+ * @tparam Itr Iterator to the range.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ *
+ * @return A pointer to the position just past the last element copied.
+ *
+ */
 
 template <typename Itr> 
 Itr copy(Itr first, Itr last, Itr d_first) {
-while(first != last){
-  *d_first = *first;
-  first++;
-  d_first++;
-}
-return d_first;
+  while(first != last){
+    *d_first = *first;
+    first++;
+    d_first++;
+  }
+  return d_first;
 }
 
-/**
- * 
- * 
- * 
- *  doxygen find_if
- * 
- * 
- * 
-*/
+/*!
+ * Finds and return a pointer to the first element in the range for which a given predicate p returns true.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam An unary predicate that will be given by the user.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param p_ Predicate which return true for the required element.
+ *
+ * @return A pointer to the first element that satisfied the condition or last, if no such element was found.
+ *
+ */
 
 template <typename Type>
 bool p (const Type &a);
 
 template <typename Itr, typename Predicate>
 Itr find_if(Itr first, Itr last, Predicate p) {
-
   Itr pointerOne = first;
   Itr pointerTwo = last;
 
@@ -105,20 +108,24 @@ Itr find_if(Itr first, Itr last, Predicate p) {
     if(p(*pointerOne)){return pointerOne;}
     pointerOne++;
   }
-
-  /*
-  * Why i'm not using the pointerTwo to return the "last" element?
-  * Nah, it's simple ash. If the pointer didn't find any occurences,
-  * it's obvius that it had percurred over all the array and it's pointing
-  * to the last element. Simple, simple.
-  */
-  return pointerOne; 
+  return last; 
 }
 
-/**
- * 
- * doxygen companion
-*/
+/*!
+ * Receive a range and a given prediacte. Returns true if the predicate returns true for all elements
+ * in the range.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Predicate A predicate unary.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param p Predicate which returns true if the element satisfies the condition.
+ *
+ * @return A boolean value true, in the case which the range is empty or if all elements, or false,
+ * if none of the elements of the range satisfies the condition.
+ *
+ */
 
 template <typename Type>
 bool p (const Type &a);
@@ -131,6 +138,22 @@ bool all_of(Itr first, Itr last, Predicate p) {
   while(pointerOne != pointerTwo && p(*pointerOne)){pointerOne++;}
   return (pointerOne == pointerTwo);  
 }
+
+/*!
+ * Receive a range and a given prediacte. Returns true if at least one element in the range satisfies
+ * the condition.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Predicate A predicate unary.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param p An unary predicate that will be given by the user.
+ *
+ * @return A boolean value true if at least one element satisfies the condition, or false,
+ * in the case which the range is empty or if none element satisfies the condition.
+ *
+ */
 
 template <typename Itr, typename Predicate>
 bool any_of(Itr first, Itr last, Predicate p) {
@@ -146,6 +169,22 @@ bool any_of(Itr first, Itr last, Predicate p) {
   return false;
 }
 
+/*!
+ * Receive a range and a given prediacte. Returns true if the predicate returns true for all elements
+ * in the range.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Predicate A predicate unary.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param p An unary predicate that will be given by the user.
+ *
+ * @return A boolean value true, in the case which the range is empty or if all elements, or false,
+ * if none of the elements of the range satisfies the condition.
+ *
+ */
+
 template <typename Itr, typename Predicate>
 bool none_of(Itr first, Itr last, Predicate p) {
   Itr pointerOne = first;
@@ -157,27 +196,52 @@ bool none_of(Itr first, Itr last, Predicate p) {
   return (pointerOne == pointerTwo);
 }
 
-/*
- *
- * EQUAL OVERLOADED FUNCTIONS
- *  
-*/
 template <typename Type>
 bool eq(const Type &a, const Type &b){
   return(a == b);
 }
 
+/*!
+ * Receives two ranges and verifies if it are equal and return the equality response.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Equal A regular equality function. Equality functor.
+ *
+ * @param first1_ Pointer to the first element of the first range (inclusive).
+ * @param last1_ Pointer to the last element of the first range (exclusive).
+ * @param first2_ Pointer to the first element of the second range (inclusive).
+ * @param eq The equality function return true if the two elements are equal and false otherwise.
+ *
+ * @return A boolean value true if the two elements correspond the equality and false if not.
+ *
+ */
+
 template <typename Itr, typename Equal>
 bool equal(Itr first1, Itr last1, Itr first2, Equal eq) {
 
-  int FullRange = last1-first1;
-  int HalfRange = first2+FullRange;
-  //Primeiro range é full range
-  //Segundo range é first2, até first2+full range
-  return eq(FullRange, HalfRange);
+  while(first1 != last1){
+    if(!eq(*first1, *first2)){return false;}
+    first1++;
+    first2++;
+  }
+  return true;
 }
 
-/*full erros*/
+/*!
+ * Receives two ranges and verifies if their elements are equal and return the equality response.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Equal A regular equality function. Equality functor.
+ *
+ * @param first1_ Pointer to the first element of the first range (inclusive).
+ * @param last1_ Pointer to the last element of the first range (exclusive).
+ * @param first2_ Pointer to the first element of the second range (inclusive).
+ * @param last2_ Pointer to the last element of the second range (exclusive).
+ * @param eq The equality function return true if the two elements are equal and false otherwise.
+ *
+ * @return A boolean value true if all the elements in the ranges are equal, and false otherwise.
+ *
+ */
 
 template <typename Itr, typename Equal>
 bool equal(Itr first1, Itr last1, Itr first2, Itr last2, Equal eq) {
@@ -192,31 +256,56 @@ bool equal(Itr first1, Itr last1, Itr first2, Itr last2, Equal eq) {
   return (pointerOne == last1);
 }
 
-/*template <typename Type>
-bool eq(const Type &a, const Type &b){
-  return (a == b);
-}*/
+/*!
+ * Receives a range of elements, rearrange the elements in the range in a way that the non-duplicated 
+ * elements are located at the beginning of the array. Returns a pointer to the address with the last element
+ * of the range with unique elements.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Equal A regular equality function. Equality functor.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param eq The equality function return true if the two elements are equal and false otherwise.
+ *
+ * @return A pointer to the address with the last element of the range with unique elements.
+ *
+ */
 
 template <typename Itr, typename Equal> 
 Itr unique(Itr first, Itr last, Equal eq) {
+  if (first == last) return last;
+
   Itr pointerOne = first;
-  Itr pointerTwo = first;
-
-  while(pointerOne != last){
-    for(auto it = first; it != pointerOne; it++){
-      if(!(eq(*pointerOne, *it))){  
-        *pointerTwo = *pointerOne;
-        pointerTwo++;
-      }
+  while (++first != last) {
+    if (!eq(*pointerOne, *first)) {
+      *(++pointerOne) = *first;
     }
-    pointerOne++;
   }
+  return ++pointerOne;
 
-  return pointerTwo;
 }
+
+/**/
 
 template <typename Type>
 bool p( const Type &a);
+
+/*!
+ * Receives a range and reorders in such way that all elements for which given preidcate p returns
+ * true precede the elements for which predicate returns false and returns a pointer to the beginning of
+ * the range which p returns false.
+ *
+ * @tparam Itr Iterator to the range.
+ * @tparam Compare A regular comparison function.Comparison functor.
+ *
+ * @param first_ Pointer to the first element of the range (inclusive).
+ * @param last_ Pointer to the last element of the range (exclusive).
+ * @param p_ An unary predicate that will be given by the user.
+ *
+ * @return A pointer to the beginning of the range which p returns false.
+ *
+ */
 
 template <typename Itr, typename Predicate>
 Itr partition(Itr first, Itr last, Predicate p) {
@@ -224,7 +313,7 @@ Itr partition(Itr first, Itr last, Predicate p) {
   Itr pointerTwo = first;
 
   while(pointerOne != last){
-    if(!p(*pointerOne)){
+    if(p(*pointerOne)){
       *pointerTwo = *pointerOne;
       pointerTwo++;
     }
